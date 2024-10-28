911fbe47566b92d929073da51f1a712ad5bf4477
Davi Feliciano <dfeliciano37@gmail.com>
2024-10-27 21:06:29 -0300

    fix: add cors fairing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use rocket::{
     fairing::AdHoc,
     figment::providers::{Format, Toml},
+    http::Method,
     launch, routes,
 };
+use rocket_cors::{AllowedOrigins, CorsOptions};
 use rocket_db_pools::Database;
 use urlessen::{
     auth::handlers::{logout, refresh, signin, signup},
@@ -13,9 +15,19 @@ use urlessen::{
 #[launch]
 fn rocket() -> _ {
     let figment = rocket::Config::figment().merge(Toml::file("App.toml").nested());
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allowed_methods(
+            vec![Method::Get, Method::Post, Method::Patch, Method::Delete]
+                .into_iter()
+                .map(From::from)
+                .collect(),
+        )
+        .allow_credentials(true);
 
     rocket::custom(figment)
         .attach(AdHoc::config::<Config>())
+        .attach(cors.to_cors().unwrap())
         .attach(Db::init())
         .mount("/auth", routes![signup, signin, refresh, logout])
 }
